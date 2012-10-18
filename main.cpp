591b708de9367d3ee4455bfbf33eb1b96c412a26
kartiklakhotia <kartiklakhotia@gmail.com>
2012-10-19 00:43:03 +0530

    filled main.cpp with fibo function

--- main.cpp
+++ main.cpp
@@ -1,1 +1,15 @@
 #include <iostream>
+
+int fibo(int n)
+{
+	fibocurrent = 1;
+	fiboprev = 1;
+	fibonext = 0;
+	for (int i=0; i<n; i++)
+	{
+		fibonext = fibocurrent + fiboprev;
+		fiboprev = fibocurrent;
+		fibocurrent = fibonext;
+	}
+	return fibocurrent;
+}
